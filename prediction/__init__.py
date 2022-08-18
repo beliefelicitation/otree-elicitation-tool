7b80ffd9d58884f5a15d185779c5f7ef250749fc
Ismael Benslimane <ismael@benslimane.fr>
2022-08-18 13:35:45 +0200

    init

---
+++ prediction/__init__.py
@@ -0,0 +1,144 @@
+from otree.api import *
+import json, numpy
+c = Currency  # old name for currency; you can delete this.
+
+
+doc = """
+Elicitation belief app
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'prediction'
+    players_per_group = None
+
+    def prediction_questions():
+
+        return [
+            dict(
+        q="What will be the temperature (°F) in New York City at noon on the 10th of September, 2022?",
+        min=60,
+        step=10,
+        nb_bins=11,
+        unit="°"),
+            dict(
+        q="20 years from now, what will be the temperature (°F) in New York City at noon on the 10th of September, 2042?",
+        min=60,
+        step=10,
+        nb_bins=11,
+        unit="°"
+            )
+        ]
+
+    num_rounds = len(prediction_questions())
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    data = models.StringField()
+    history = models.LongStringField(initial="[]")
+    min = models.IntegerField(label="Min value of the x axis")
+    max = models.IntegerField(label="Max value of the x axis")
+    nb_bins = models.IntegerField(label="Number of bins", min=2)
+
+
+def creating_session(subsession: Subsession):
+    if 'interface' in subsession.session.config:
+        for player in subsession.get_players():
+            player.interface = subsession.session.config["interface"]
+            player.participant.interface = player.interface
+    else:
+        import itertools
+        interfaces = itertools.cycle(["ours", "number","bins","metaculus"])
+        for player in subsession.get_players():
+            player.interface = next(interfaces)
+            player.participant.interface = player.interface
+
+# PAGES
+
+class End(Page):
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == Constants.num_rounds
+
+
+class Prediction(Page):
+    form_model = 'player'
+    form_fields = ["min","max","nb_bins"]
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(
+            prediction_questions = Constants.prediction_questions()[player.round_number-1]["q"],
+        )
+
+    @staticmethod
+    def error_message(player, values):
+        if values['max'] <= values['min']:
+            return 'Max value should be superior to Min value!'
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.session.config["self"] == True
+
+class Distributions(Page):
+
+    form_model = 'player'
+    form_fields = []
+
+    timer_text = 'Time left to make your prediction:'
+
+    @staticmethod
+    def get_timeout_seconds(player):
+        if player.session.config["timeout"]:
+            return 90
+
+    @staticmethod
+    def live_method(player, data):
+        history = json.loads(player.history)
+        history.append(data["history"])
+        player.history = json.dumps(history)
+        player.data = json.dumps(data["history"]["data"])
+
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        pass
+#        if timeout_happened:
+#            player.timeout = True
+
+
+    @staticmethod
+    def js_vars(player):
+        if not player.session.config["self"]:
+            player.min = Constants.prediction_questions()[player.round_number-1]["min"]
+            player.nb_bins = Constants.prediction_questions()[player.round_number-1]["nb_bins"]
+            player.max = Constants.prediction_questions()[player.round_number-1]["min"]+Constants.prediction_questions()[player.round_number-1]["step"]*(Constants.prediction_questions()[player.round_number-1]["nb_bins"]-1)
+            print(player.max)
+
+        return dict(
+            interface = player.participant.interface,
+            prediction = True,
+            yMax = 1,
+            min = player.min,
+            step = (player.max-player.min)/(player.nb_bins-1),
+            nb_bins = player.nb_bins,
+            xUnit = Constants.prediction_questions()[player.round_number-1]["unit"],
+            min_timeout=30,
+        )
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(
+            prediction_questions = Constants.prediction_questions()[player.round_number-1]["q"],
+            interface=player.participant.interface,
+        )
+
+page_sequence = [Prediction,Distributions,End]
---
+++ settings.py
@@ -0,0 +1,121 @@
+from os import environ
+
+
+SESSION_CONFIGS = [
+    dict(
+        name='drag_self',
+        display_name="Drag and drop interface (player set min,max,nbins)",
+        app_sequence=['prediction'],
+        beta=False,
+        interface="ours",
+        timeout=False,
+        num_demo_participants=1,
+        self=True
+    ),
+    dict(
+        name='drag',
+        display_name="Drag and drop interface",
+        app_sequence=['prediction'],
+        beta=False,
+        interface="ours",
+        timeout=False,
+        num_demo_participants=1,
+        self=False
+    ),
+    dict(
+        name='elicitation_belief_number_timeout_self',
+        display_name="Number-input interface (player set min,max,nbins)",
+        app_sequence=['prediction'],
+        beta=False,
+        interface="number",
+        timeout=False,
+        num_demo_participants=1,
+        self=True
+    ),
+    dict(
+        name='elicitation_belief_number_timeout',
+        display_name="Number-input interface",
+        app_sequence=['prediction'],
+        beta=False,
+        interface="number",
+        timeout=False,
+        num_demo_participants=1,
+        self=False
+    ),
+    dict(
+        name='elicitation_belief_slider_not_norm_timeout_self',
+        display_name="Slider-input interface (player set min,max,nbins)",
+        app_sequence=['prediction'],
+        beta=False,
+        interface="bins",
+        timeout=False,
+        num_demo_participants=1,
+        self=True
+    ),
+    dict(
+        name='elicitation_belief_slider_not_norm_timeout',
+        display_name="Slider-input interface",
+        app_sequence=['prediction'],
+        beta=False,
+        interface="bins",
+        timeout=False,
+        num_demo_participants=1,
+        self=False
+    ),
+    dict(
+        name='elicitation_belief_metaculus_timeout_self',
+        display_name="The metaculus normal distribution (player set min,max,nbins)",
+        app_sequence=['prediction'],
+        beta=False,
+        interface="metaculus",
+        timeout=False,
+        num_demo_participants=1,
+        self=True
+    ),
+    dict(
+        name='elicitation_belief_metaculus_timeout',
+        display_name="The metaculus normal distribution",
+        app_sequence=['prediction'],
+        beta=False,
+        interface="metaculus",
+        timeout=False,
+        num_demo_participants=1,
+        self=False
+    ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+PARTICIPANT_FIELDS = ["interface"]
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.5,  doc=""
+)
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = False
+
+ROOMS = [
+]
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+
+
+DEMO_PAGE_INTRO_HTML = """
+<a class="btn btn-primary" href="https://beliefelicitation.github.io/">Go to the main website</a>
+"""
+
+
+SECRET_KEY = environ.get('OTREE_SECRET_KEY')
+
+INSTALLED_APPS = ['otree']
